d6a2b030e280631287249a837b146c8a4dd41928
Navyagolash <golashnavya@gmail.com>
2026-06-24 23:25:48 +0530

    modified on server

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ console.log("Pass:", process.env.EMAIL_PASS ? "Loaded" : "Missing");
 const app = express();
 app.use(express.json());
 app.use(cors({
-  origin: "*"  // or  its will be there like that ["https://your-vercel-app.vercel.app"]
+  origin: "*"  
 }));
 
 mongoose.connect("mongodb+srv://logindb:testdb@cluster0.z7wzh1e.mongodb.net/loginDB")
